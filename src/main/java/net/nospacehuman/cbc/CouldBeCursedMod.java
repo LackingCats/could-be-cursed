fa2a092c9470b7733bd2234b6e3c302618a70692
lackingcats <jackson.marsden6@gmail.com>
2022-12-19 12:39:59 +1100

    ynl

--- src/main/java/net/nospacehuman/cbc/CouldBeCursedMod.java
+++ src/main/java/net/nospacehuman/cbc/CouldBeCursedMod.java
@@ -5,11 +5,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CouldBeCursedMod implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+	public static final String MOD_ID = "cbc";
+	public static final String MOD_NAME = "Could Be Cursed";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info(MOD_NAME + " / " + MOD_ID);
 	}
 }
