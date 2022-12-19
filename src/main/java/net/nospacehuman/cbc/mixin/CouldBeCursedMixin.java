f0bcc79d049f1a90c1e3786f41664acb6653e6c1
lackingcats <jackson.marsden6@gmail.com>
2022-12-19 12:49:21 +1100

    test_project

--- src/main/java/net/nospacehuman/cbc/mixin/CouldBeCursedMixin.java
+++ src/main/java/net/nospacehuman/cbc/mixin/CouldBeCursedMixin.java
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class CouldBeCursedMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		CouldBeCursedMod.LOGGER.info("This line is printed by an example mod mixin!");
+		CouldBeCursedMod.LOGGER.info("mixin_mixin");
 	}
 }
