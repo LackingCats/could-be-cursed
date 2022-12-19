a909dfad4893a46bc8634241d6f1effcda8d30fd
lackingcats <jackson.marsden6@gmail.com>
2022-12-19 12:30:22 +1100

    Initial commit

---
+++ src/main/java/net/nospacehuman/cbc/mixin/CouldBeCursedMixin.java
@@ -0,0 +1,16 @@
+package net.nospacehuman.cbc.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.nospacehuman.cbc.CouldBeCursedMod;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class CouldBeCursedMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		CouldBeCursedMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
